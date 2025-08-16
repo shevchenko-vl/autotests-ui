d9d3472d3d0f723e6fea7a0ad8275602141f1b98
shevchenko-vl <shevchenko-vl@test.org>
2025-08-16 17:42:43 +0300

    Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -57,3 +57,48 @@ class TestCourses:
             max_score=new_course['max_score'],
             min_score=new_course['min_score']
         )
+
+    def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+        create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+        create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+        new_course = {
+            'title': 'Playwright2',
+            'estimated_time': '21 weeks',
+            'description': 'Playwright2',
+            'max_score': '42',
+            'min_score': '21',
+        }
+        create_course_page.create_course_form.fill(**new_course)
+        create_course_page.create_course_toolbar.click_create_course_button()
+
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title=new_course['title'],
+            estimated_time=new_course['estimated_time'],
+            max_score=new_course['max_score'],
+            min_score=new_course['min_score']
+        )
+
+        courses_list_page.course_view.menu.click_edit(index=0)
+        create_course_page.create_course_form.check_visible(**new_course)
+
+        edited_course = {
+            'title': 'Playwright2_edited',
+            'estimated_time': '42 weeks',
+            'description': 'Playwright2 edited',
+            'max_score': '43',
+            'min_score': '22',
+        }
+        create_course_page.create_course_form.fill(**edited_course)
+        create_course_page.create_course_form.check_visible(**edited_course)
+        create_course_page.create_course_toolbar.click_create_course_button()
+
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title=edited_course['title'],
+            estimated_time=edited_course['estimated_time'],
+            max_score=edited_course['max_score'],
+            min_score=edited_course['min_score']
+        )
