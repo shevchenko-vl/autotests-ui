64fb8cf06f02e9bd60b1728213cdf5a4bd681dec
shevchenko-vl <shevchenko-vl@test.org>
2025-08-18 15:49:04 +0300

    Добавление Allure шагов в автотесты

--- components/views/empty_view_component.py
+++ components/views/empty_view_component.py
@@ -1,4 +1,5 @@
-from playwright.sync_api import Page, expect
+import allure
+from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
 from elements.icon import Icon
@@ -13,6 +14,7 @@ class EmptyViewComponent(BaseComponent):
         self.title = Text(page, f'{identifier}-empty-view-title-text', 'Title')
         self.description = Text(page, f'{identifier}-empty-view-description-text', 'Description')
 
+    @allure.step('Check visible empty view "{title}"')
     def check_visible(self, title: str, description: str):
         self.icon.check_visible()
 
